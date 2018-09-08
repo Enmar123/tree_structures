35105fafea831ebf4c9535e1b0a199a8c548379e
Enmar123 <js_enmar1@hotmail.com>
2018-09-08 13:24:19 -0400

    v0.0.0.1 Bintree test commit

---
+++ bintree.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  8 12:02:06 2018
+
+@author: js_en
+"""
+
+""" in this code we will attempt to create a binary tree capable
+    of storing objects
+"""
+import numpy as np
+# User Var
+
+class Node:
+    
+    
+    
+    def __init__(self,):
+
+if __name__=="__main__":
+    
+    
